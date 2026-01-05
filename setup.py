bdf00f677705fa2ceca1d59c880618f59014308c
SeaMinh <seaminh.smh@gmail.com>
2026-01-05 08:42:52 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyunluac",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+    description="Python library to decompile Lua bytecode (.luac)",
+    author="Your Name",
+)
